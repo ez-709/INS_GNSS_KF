5f35418fe672e391bded346c5ea939ddbccde63f
ez-709 <egor.zhuravlev0407@gmail.com>
2026-04-06 12:05:47 +0300

    main_fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 #include "imu_parser.hpp"
 #include "gnss_parser.hpp"
 #include "logger.hpp"
+#include <unistd.h>
 
 int main() {
     IMU_parser imu;
     GNSS_parser gnss;
     Logger logger;
 
-    IMU_data imu_data = imu.read();
-    GNSS_data gnss_data = gnss.read();
-    logger.write(imu_data);
-    logger.write(gnss_data);
+    while (true) {
+        IMU_data imu_data = imu.read();
+        GNSS_data gnss_data = gnss.read();
+        logger.write(imu_data);
+        logger.write(gnss_data);
+        usleep(10000);
+    }
+
     return 0;
 }
