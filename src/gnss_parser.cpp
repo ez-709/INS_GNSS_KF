a55232c6735ce61ef52261bd1346becef380e531
ez_709 <egor.zhuravlev0407@gmail.com>
2026-05-06 00:52:12 +0300

    fixes

--- src/gnss_parser.cpp
+++ src/gnss_parser.cpp
@@ -9,6 +9,7 @@
 GNSS_parser::GNSS_parser() {
     fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NONBLOCK);
     if (fd < 0) return;
+    
     struct termios tty = {};
     cfsetispeed(&tty, B115200);
     cfsetospeed(&tty, B115200);
@@ -18,42 +19,59 @@ GNSS_parser::GNSS_parser() {
 
 GNSS_data GNSS_parser::read() {
     if (fd < 0) return last;
-
-    static char buf[256];
+    
+    static char buf[512];
     static int  len = 0;
-
+    
     int n = ::read(fd, buf + len, sizeof(buf) - len - 1);
     if (n > 0) len += n;
     buf[len] = '\0';
-
+    
     char* nl;
     char* p = buf;
+    
     while ((nl = strchr(p, '\n')) != nullptr) {
         *nl = '\0';
         if (strncmp(p, "$GNRMC", 6) == 0 || strncmp(p, "$GPRMC", 6) == 0) {
-            char tmp[128]; strncpy(tmp, p, 127);
-            char* f[12] = {}; int fi = 0;
+            char tmp[128]; 
+            strncpy(tmp, p, 127);
+            tmp[127] = '\0';
+            
+            char* f[12] = {}; 
+            int fi = 0;
             for (char* t = strtok(tmp, ",*"); t && fi < 12; t = strtok(nullptr, ",*"))
                 f[fi++] = t;
-            if (fi >= 9 && f[2][0] == 'A') {
+            
+            if (fi >= 9 && f[2] && f[2][0] == 'A') {
                 auto nmea2deg = [](const char* s) {
                     double r = atof(s);
                     int d = (int)(r / 100);
                     return d + (r - d * 100) / 60.0;
                 };
-                last.lot = nmea2deg(f[3]); if (f[4][0]=='S') last.lot=-last.lot;
-                last.lon = nmea2deg(f[5]); if (f[6][0]=='W') last.lon=-last.lon;
-                double spd = atof(f[7]) * 0.51444;
-                double hdg = atof(f[8]) * M_PI / 180.0;
-                last.VE = spd * sin(hdg);
-                last.VN = spd * cos(hdg);
+                
+                if (f[3] && f[4] && f[5] && f[6]) {
+                    last.lot = nmea2deg(f[3]); 
+                    if (f[4][0] == 'S') last.lot = -last.lot;
+                    
+                    last.lon = nmea2deg(f[5]); 
+                    if (f[6][0] == 'W') last.lon = -last.lon;
+                }
+                
+                if (f[7] && f[8]) {
+                    double spd = atof(f[7]) * 0.51444;
+                    double hdg = atof(f[8]) * M_PI / 180.0;
+                    last.VE = spd * sin(hdg);
+                    last.VN = spd * cos(hdg);
+                }
             }
         }
+        
         p = nl + 1;
     }
+    
     int rem = len - (int)(p - buf);
     if (rem > 0) memmove(buf, p, rem);
     len = rem;
-
+    
     return last;
 }
